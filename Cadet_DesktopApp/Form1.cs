9db4c1e4b85b25ba4b8968ba84829575413d4742
Akhil Raj Azhikodan <akhil95raj@gmail.com>
2015-11-14 01:03:16 +0530

    Created C# form application
    updates:
    1) Button (scan finger ) created , which executes ErollFingerfromScanner
    2) Successfull Scanning of the finger
    3) filename : CadetEnrolFingerTemplate , CadetEnrollFingerImage
    
    Note: please make sure  that the project has to be edited with the path
    to the neurotechnology SDK , eg:
    Neurotec_Biometric_6_0_SDK_Trial\Bin\Win64_x64

---
+++ Cadet_DesktopApp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace Cadet_DesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"D:\Projects\CADET\Neurotec_Biometric_6_0_SDK_Trial\Bin\Win64_x64\EnrollFingerFromScannerCS.exe");
+        }
+    }
+}
